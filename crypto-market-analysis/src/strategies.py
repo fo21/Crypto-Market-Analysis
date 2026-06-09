6e1ac2f65f4d5731561ece2d38c55d197d21556a
Smaranda Tasmoc <98458590+fo21@users.noreply.github.com>
2026-06-09 17:54:08 +0300

    Add mean reversion function

--- crypto-market-analysis/src/strategies.py
+++ crypto-market-analysis/src/strategies.py
@@ -10,3 +10,16 @@ def momentum_strategy(price):
 
     return signal
 
+def mean_reversion(price):
+
+    rolling_mean = price.rolling(20).mean()
+
+    rolling_std = price.rolling(20).std()
+
+    zscore = (
+        price - rolling_mean
+    ) / rolling_std
+
+    signal = (zscore < -1).astype(int)
+
+    return signal
